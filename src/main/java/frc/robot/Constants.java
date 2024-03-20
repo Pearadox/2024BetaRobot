a4c44c4df76d82e1cb474f8898a605c3f6c2fd54
myra <myrajurek@gmail.com>
2024-03-19 19:54:56 -0500

    Initial Commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,101 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+  
+  public static final class SwerveConstants {
+    //Drivetrain motor/encoder IDs
+    public static final int LEFT_FRONT_DRIVE_ID = 1;
+    public static final int RIGHT_FRONT_DRIVE_ID = 2;
+    public static final int LEFT_BACK_DRIVE_ID = 3;
+    public static final int RIGHT_BACK_DRIVE_ID = 4;
+    
+    public static final int LEFT_FRONT_TURN_ID = 5;
+    public static final int RIGHT_FRONT_TURN_ID = 6;
+    public static final int LEFT_BACK_TURN_ID = 7;
+    public static final int RIGHT_BACK_TURN_ID = 8;
+    
+    public static final int LEFT_FRONT_CANCODER_ID = 11;
+    public static final int RIGHT_FRONT_CANCODER_ID = 12;
+    public static final int LEFT_BACK_CANCODER_ID = 13;
+    public static final int RIGHT_BACK_CANCODER_ID = 14;
+
+    public static final int PIGEON_ID = 15;
+    
+    //Drivetrain characteristics
+    //All of these need to be changed
+    public static final double LEFT_FRONT_OFFSET = 0.307;
+    public static final double RIGHT_FRONT_OFFSET = -0.254;
+    public static final double LEFT_BACK_OFFSET = 0.026;
+    public static final double RIGHT_BACK_OFFSET = 0.144;
+
+    public static final double WHEEL_DIAMETER = Units.inchesToMeters(4);
+    public static final double DRIVE_MOTOR_GEAR_RATIO = 5.357; //SDS Mk4i L3+ (60:16 first stage)
+    public static final double TURN_MOTOR_GEAR_RATIO = 150.0/7;
+    public static final double DRIVE_MOTOR_PCONVERSION = WHEEL_DIAMETER * Math.PI / DRIVE_MOTOR_GEAR_RATIO;
+    public static final double TURN_MOTOR_PCONVERSION = 2 * Math.PI / TURN_MOTOR_GEAR_RATIO;
+    public static final double DRIVE_MOTOR_VCONVERSION = DRIVE_MOTOR_PCONVERSION;
+    public static final double TURN_MOTOR_VCONVERSION = TURN_MOTOR_PCONVERSION / 60.0;
+    public static final double KP_TURNING = 0.5;
+
+    public static final double kS_PERCENT = 0.035;
+    public static final double kP_PERCENT = 0.020;
+    ;
+
+    public static final double DRIVETRAIN_MAX_SPEED = 6.62;
+    public static final double DRIVETRAIN_MAX_ANGULAR_SPEED = 3.45 * Math.PI; //TODO: Determine max angular speed
+
+    //Swerve Kinematics
+    //These also need to be changed
+    public static final double TRACK_WIDTH = Units.inchesToMeters(24.75);
+    public static final double WHEEL_BASE = Units.inchesToMeters(22.75);
+    public static final double DRIVE_BASE_RADIUS = Math.sqrt(Math.pow(TRACK_WIDTH, 2) + Math.pow(WHEEL_BASE, 2)) / 2.0;
+
+    public static final SwerveDriveKinematics DRIVE_KINEMATICS = new SwerveDriveKinematics(
+        new Translation2d(WHEEL_BASE / 2, TRACK_WIDTH / 2),
+        new Translation2d(WHEEL_BASE / 2, -TRACK_WIDTH / 2),
+        new Translation2d(-WHEEL_BASE / 2, TRACK_WIDTH / 2),
+        new Translation2d(-WHEEL_BASE / 2, -TRACK_WIDTH / 2)
+    );
+
+    //Teleop constraints
+    public static final double TELE_DRIVE_MAX_SPEED = DRIVETRAIN_MAX_SPEED;
+    public static final double TELE_DRIVE_MAX_ANGULAR_SPEED = DRIVETRAIN_MAX_ANGULAR_SPEED / 1.75;
+    public static final double TELE_DRIVE_MAX_ACCELERATION = 3;
+    public static final double TELE_DRIVE_MAX_ANGULAR_ACCELERATION = 3;
+
+    //Auton constrains
+    public static final double AUTO_kP_TRANSLATION = 4;
+    public static final double AUTO_kP_ROTATION = 1.5;
+    
+        public static final HolonomicPathFollowerConfig AUTO_CONFIG = new HolonomicPathFollowerConfig(
+      new PIDConstants(AUTO_kP_TRANSLATION, 0.0, 0.0),
+      new PIDConstants(AUTO_kP_ROTATION, 0.0, 0.0),
+      DRIVETRAIN_MAX_SPEED, // Max module speed, in m/s
+      DRIVE_BASE_RADIUS,
+      new ReplanningConfig());
+  }
+  
+}
